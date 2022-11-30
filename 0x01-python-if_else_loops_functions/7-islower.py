d2f5f0ef1f02153eb462a4cf45f1e2ab782a845d
kmazengia <kefecoder@gmail.com>
2022-11-30 02:16:23 +0000

    correcting erros

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-def islower(c)
-    if ord(c) >= 97 and ord(c) <= 122:
+def islower(a)
+    if ord(a) >= 97 and ord(a) <= 122:
         return(True)
     else:
         return(False)
