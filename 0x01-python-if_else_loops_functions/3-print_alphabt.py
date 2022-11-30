b5f53653c54a47aa929c277f54feaa40feb95eb4
kmazengia <kefecoder@gmail.com>
2022-11-30 01:37:02 +0000

    adding a python script that  prints alphabet without q and e

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,4 @@
-#!user/bin/python3
-for ch in range(97, 123) 
-in range(97, 123):i
+#!/usr/bin/python3
+for ch in range(97, 123):
         if ch != 101 and ch != 113:
                 print("{:c}".format(ch), end='')
