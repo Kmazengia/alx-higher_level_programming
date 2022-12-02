9492f12193c01340f94a67ebaab1da1f5b7812a8
kmazengia <kefecoder@gmail.com>
2022-12-01 18:06:30 -0800

    hidden_discovery

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
-def magic_calculation(a, b):
-    return 98 + a ** b
+if __name__ == "__main__":
+    import hidden_4
+    for name in dir(hidden_4):
+        if name[0] != '_' and name[1] != '_':
+            print(name)i
