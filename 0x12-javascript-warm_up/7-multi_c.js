c6f83bb00c19615da6fa7d6bce206b09db94e1e4
kmazengia <kefecoder@gmail.com>
2023-03-30 06:45:09 -0700

    multi_c

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+let x = parseInt(process.argv[2]);
+if (Number.isNaN(x)) {
+  console.log('Missing number of occurrences');
+} else {
+  while (x > 0) {
+    console.log('C is fun');
+    x--;
