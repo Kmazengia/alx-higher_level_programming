adc3f198e56e6ed393744e2c65b925d600056994
kmazengia <kefecoder@gmail.com>
2023-04-03 02:25:38 -0700

    catch me

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-# Write a Bash script that makes a request to 0.0.0.0:5000/catch_me that causes the server to respond with a message containing You got me!, in the body of the response.
+# Write a Bash script that makes a request to 0.0.0.0:5000/catch_me 
+# that causes the server to respond with a message containing You got me!, in the body of the response.
 curl -sL 0.0.0.0:5000/catch_me -X PUT -H "Origin:You got me!"
